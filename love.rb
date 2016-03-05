01ddd57252dab10d29e5502805dbfe55188ea05d
Sarika Dani <monkey@Sarikas-MacBook-Air.local>
2016-03-05 17:03:57 -0500

    I finished another

--- love.rb
+++ love.rb
@@ -3,22 +3,26 @@ require 'pry'
 
 # define a function that accepts two parameters:
 	# name and what_you_love
-	# the function should be called captain_obvious 
+	def captain_obvious(name, what_you_love)
+		puts "My name is #{name} and I love #{what_you_love}!"
+	end
+	# the function should be called captain_obvious
 	# if I called the function with "Jay" and "Football" it should say:
 		 #My name is Jay and I love Football!
 
 
 
-# Show the user a question that says "what is your name?" 
+# Show the user a question that says "what is your name?"
+puts "what is your name?"
 
-
-# Store the user's input in a variable 
-
+# Store the user's input in a variable
+yourname = gets.strip
 
 # Show the user a question that says "What do you love?"
+puts "what do you love?"
 
+# Store the user's input in a variable
+thingyoulove = gets.strip
 
-# Store the user's input in a variable 
-
-
-# Call the function 
+# Call the function
+captain_obvious(yourname, thingyoulove)
