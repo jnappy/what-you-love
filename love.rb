64507731acd93c76bef77fc3357d3e19e90495fb
501547952 <a501547952@n30501547952ml.eng.interplay3.prod>
2016-03-05 17:01:31 -0500

    this is what i love

--- love.rb
+++ love.rb
@@ -1,24 +1,28 @@
 require 'pry'
 
+def captain_obvious (name,love)
+	puts "my name is #{name} and i love #{love}!"
+end
 
 # define a function that accepts two parameters:
 	# name and what_you_love
-	# the function should be called captain_obvious 
+	# the function should be called captain_obvious
 	# if I called the function with "Jay" and "Football" it should say:
 		 #My name is Jay and I love Football!
 
 
 
-# Show the user a question that says "what is your name?" 
+# Show the user a question that says "what is your name?"
+puts "what is your name"
 
-
-# Store the user's input in a variable 
-
+# Store the user's input in a variable
+yourname = gets.strip
 
 # Show the user a question that says "What do you love?"
+puts "what do you love"
 
+# Store the user's input in a variable
+yourlove = gets.strip
 
-# Store the user's input in a variable 
-
-
-# Call the function 
+# Call the function
+captain_obvious(yourname, yourlove)
