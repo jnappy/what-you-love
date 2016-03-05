472c920aa44a1ee105b82ff14af74fffbcb52b04
Dev Eggers <deveggers@Brendans-MacBook-Pro.local>
2016-03-05 16:59:57 -0500

    Made the captain_obvious function

--- love.rb
+++ love.rb
@@ -3,22 +3,37 @@ require 'pry'
 
 # define a function that accepts two parameters:
 	# name and what_you_love
-	# the function should be called captain_obvious 
+	# the function should be called captain_obvious
 	# if I called the function with "Jay" and "Football" it should say:
 		 #My name is Jay and I love Football!
+#without parameters
 
 
+#def captain_obvious()
+#	puts "What is your name?"
+#	name = gets.strip.downcase.capitalize
+#	puts "What do you love"
+#	what_you_love = gets.strip
+#	puts "My name is #{name} and I love #{what_you_love}"
+#end
 
-# Show the user a question that says "what is your name?" 
+
+def captain_obvious(name, what_you_love)
+	puts "My name is #{name} and I love #{what_you_love}"
+end
 
 
-# Store the user's input in a variable 
+# Show the user a question that says "what is your name?"
+puts "what is your name?"
 
+# Store the user's input in a variable
+name = gets.strip.downcase.capitalize
 
 # Show the user a question that says "What do you love?"
+puts "What do you love?"
 
+# Store the user's input in a variable
+what_you_love = gets.strip
 
-# Store the user's input in a variable 
-
-
-# Call the function 
+# Call the function
+captain_obvious(name,what_you_love)
